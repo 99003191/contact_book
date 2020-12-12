df7dc7a73fc894a8772dc6d6370d4e187f83058d
99003191 <75427938+99003191@users.noreply.github.com>
2020-12-13 00:19:33 +0530

    Update and rename Untitled1.py to Activity1.py

--- Activity1.py
+++ Activity1.py
@@ -1,15 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
 
 class contact_menu:
     
     def __init__ (self):
         self.opt = 0
         print("*******Contact Book********")
-    # Menu function
 
 
     def menu(self): 
